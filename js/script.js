a5b849a1ed479aeece44e54e434b2d5e55113fd9
Daniele <danielefontana90@gmail.com>
2020-05-22 07:30:14 +0200

    Event Click Biglietto

--- js/script.js
+++ js/script.js
@@ -3,16 +3,38 @@ var buttonAnnulla = document.getElementById('form-annulla');
 var formNome = document.getElementById('form-nome');
 var formKm = document.getElementById('form-km');
 var formEta = document.getElementById('form-eta');
-
 //elementi Biglietto
 var biglNome = document.getElementById('bigl-nome');
-//altre variabili
-var formKmValue;
-var formEtaValue;
+var randomCarrozza = document.getElementById('bigl-carrozza');
+var randomCp = document.getElementById('bigl-codicecp');
+var offerta = document.getElementById('bigl-offerta');
+var contenitore = document.getElementById('biglietto');
+var costo = document.getElementById("bigl-costo");
+
 //evento clicca su Genera
 buttonGenera.addEventListener('click',
   function()  {
+    var prezzo = formKm.value * 0.21;
+    var sconto20 = prezzo * 20 / 100;
+    var sconto40 = prezzo * 40 / 100;
+
+    biglietto.classList.remove('hidden');
     formKmValue = formKm.value;
-    alert('ciao');
+    formEtaValue = formEta.value;
+    //comnpilazione biglietto
+    biglNome.innerHTML = formNome.value;
+    randomCarrozza.innerHTML = Math.floor(Math.random() * 10);
+    randomCp.innerHTML = Math.floor(Math.random() * 10);
+
+    if (formEtaValue === 'maggiorenne') {
+      offerta.innerHTML = "PREZZO NORMALE";
+      costo.innerHTML = prezzo;
+    }else if (formEtaValue === 'minorenne') {
+      offerta.innerHTML = "SCONTO SILVER";
+      costo.innerHTML = (prezzo - sconto20).toFixed(2);
+    }else if (formEtaValue === 'over') {
+      offerta.innerHTML = "SCONTO GOLD";
+      costo.innerHTML = (prezzo - sconto40).toFixed(2);
+    }
   }
 );
